310432e6cd3a657ebf1fa28888eebfb76e4e81c4
manzaero <ligoero@gmail.com>
2024-03-15 00:24:48 +0300

    adding cart info

--- src/components/TheCartItem.vue
+++ src/components/TheCartItem.vue
@@ -1,11 +1,12 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <div class="border-sky-100 border p-4 rounded-xl flex gap-4 items-center">
+    <img class="w-16 h-16" src="/sneakers/sneakers-1.jpg" alt="Sneakers">
+    <div class="flex flex-col">
+      <p>Кроссовки Nike Air Max 270</p>
+      <div class="flex justify-between mt-2">
+        <b>12990 руб.</b>
+        <img class="opacity-40 hover:opacity-100 cursor-pointer transition" @click="console.log(111)" src="/close.svg" alt="">
+      </div>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
--- src/components/TheDrawer.vue
+++ src/components/TheDrawer.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
-  <div class="bg-white w-1/4 h-full fixed right-0 top-0 z-20 p-6">
+  <div class="bg-white w-1/5 h-full fixed right-0 top-0 z-20 p-6">
     <the-drawer-head/>
+    <the-cart-item/>
   </div>
+
 </template>
+
 <script setup>
 import TheDrawerHead from "@/components/TheDrawerHead.vue";
+import TheCartItem from "@/components/TheCartItem.vue";
 </script>
