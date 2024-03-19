63afe24f0b6f3867e4fd886e8a467737883d3325
manzaero <ligoero@gmail.com>
2024-03-20 00:23:50 +0300

    refact code from sort

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,29 @@
 import TheHeader from "@/components/TheHeader.vue";
 import TheCardList from "@/components/TheCardList.vue";
 import TheDrawer from "@/components/TheDrawer.vue";
-import {onMounted, ref} from "vue";
+import {onMounted, ref, watch} from "vue";
 import axios from "axios";
 
 const items = ref([])
-onMounted(async() => {
+const sortBy = ref('')
+const searchQuery = ref('')
+
+const onChange = (event) => {
+  sortBy.value = event.target.value
+}
+
+onMounted(async ()=> {
   try {
-    const {data} = await axios
-        .get('https://f1d654f1728ec1d0.mokky.dev/items')
+    const {data} = await axios.get('https://f1d654f1728ec1d0.mokky.dev/items')
     items.value = data
   } catch (e) {
-    console.log(e.message)
+    console.log(e)
   }
+})
+
+watch(sortBy, async () => {
+  const {data} = await axios.get('https://f1d654f1728ec1d0.mokky.dev/items?sortBy=' + sortBy.value)
+  items.value = data
 })
 </script>
 
@@ -29,17 +40,21 @@ onMounted(async() => {
         <h2 class="text-3xl font-bold lg:text-xl sm:text-xs">Все кроссовки</h2>
 
         <div class="flex justify-between gap-4 lg:gap-1">
-          <select class="rounded-md py-2 px-3 border outline-none lg:h-8 lg:py-0 lg:px-1 md:hidden">
-            <option>По названию</option>
-            <option>Сначала дешёвые</option>
-            <option>Сначала дорогие</option>
+          <select
+              @change="onChange"
+              class="rounded-md py-2 px-3 border outline-none bg-white lg:h-8 lg:py-0 lg:px-1 md:hidden">
+            <option value="name">По названию</option>
+            <option value="price">Сначала дешёвые</option>
+            <option value="-price">Сначала дорогие</option>
           </select>
-
+          {{sortBy}}
           <div class="relative sm:p-0">
             <img class="absolute left-3 top-3 lg:left-2 lg:top-2 sm:hidden" src="/search.svg" alt="">
             <input
                 placeholder="Поиск"
-                class="border rounded-md py-2 pl-10 pr-4 outline-none focus:border-gray-400 lg:h-8 lg:py-0 lg:pl-7 lg:pr-2 sm:max-w-20 sm:p-2 sm:text-sm">
+                class="border rounded-md py-2 pl-10 pr-4 outline-none focus:border-gray-400 lg:h-8 lg:py-0 lg:pl-7 lg:pr-2 sm:max-w-20 sm:p-2 sm:text-sm"
+                v-model="searchQuery">
+            {{searchQuery}}
           </div>
         </div>
       </div>
